8275cbabc4b9428026b72adad0a141726a86f51e
Vinicius Rocha <vnc.chelsea@gmail.com>
2019-01-18 01:00:29 -0200

    [financial] refactor(utils/goals): clean code

--- financial_targets.api/src/api/utils/goals.js
+++ financial_targets.api/src/api/utils/goals.js
@@ -2,11 +2,16 @@ import enumerators from './enumerators';
 import functions from './functions';
 
 const getGoalType = targetDate => {
-  const { type } = enumerators.goals;
-  const dateDifferences = enumerators.others.date.differences;
-  const currentDate = functions.getCurrentDate();
+  const {
+    goals: { type },
+    others: {
+      date: { differences }
+    }
+  } = enumerators;
+
   const targetDateCompare = functions.createMomentDate(targetDate);
-  const daysDifference = functions.getDateDifference(dateDifferences.days, targetDateCompare, currentDate);
+  const daysDifference = functions.getDateDifference(differences.days, targetDateCompare, functions.getCurrentDate());
+
   const goalType = do {
     if (daysDifference >= 0 && daysDifference <= 60) type.shortRun;
     else if (daysDifference > 60 && daysDifference <= 365) type.middleRun;
