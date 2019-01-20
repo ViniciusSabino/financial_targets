56ff5a35da5a285255dd4e3dd0be320a295496fc
Vinicius Rocha <vnc.chelsea@gmail.com>
2019-01-20 11:35:04 -0200

    [financial] refactor(goals) tipos de metas

--- financial_targets.api/src/api/utils/enumerators/goals.js
+++ financial_targets.api/src/api/utils/enumerators/goals.js
@@ -1,9 +1,10 @@
 const goal = {
-  type: {
-    shortRun: 'SHORT_RUN',
-    middleRun: 'MIDDLE_RUN',
-    longRun: 'LONG_RUN'
-  }
+    type: {
+        veryShortRun: "VERY_SHORT_RUN",
+        shortRun: "SHORT_RUN",
+        middleRun: "MIDDLE_RUN",
+        longRun: "LONG_RUN"
+    }
 };
 
 export default goal;
--- financial_targets.api/src/api/utils/goals.js
+++ financial_targets.api/src/api/utils/goals.js
@@ -1,26 +1,31 @@
-import enumerators from './enumerators';
-import functions from './functions';
+import enumerators from "./enumerators";
+import functions from "./functions";
 
 const getGoalType = targetDate => {
-  const {
-    goals: { type },
-    others: {
-      date: { differences }
-    }
-  } = enumerators;
+    const {
+        goals: { type },
+        others: {
+            date: { differences }
+        }
+    } = enumerators;
 
-  const targetDateCompare = functions.createMomentDate(targetDate);
-  const daysDifference = functions.getDateDifference(differences.days, targetDateCompare, functions.getCurrentDate());
+    const targetDateCompare = functions.createMomentDate(targetDate);
+    const daysDifference = functions.getDateDifference(
+        differences.days,
+        targetDateCompare,
+        functions.getCurrentDate()
+    );
 
-  const goalType = do {
-    if (daysDifference >= 0 && daysDifference <= 60) type.shortRun;
-    else if (daysDifference > 60 && daysDifference <= 365) type.middleRun;
-    else type.longRun;
-  };
+    const goalType = do {
+        if (daysDifference >= 0 && daysDifference <= 92) type.veryShortRun;
+        else if (daysDifference > 92 && daysDifference <= 730) type.shortRun;
+        else if (daysDifference > 730 && daysDifference <= 1825) type.middleRun;
+        else type.longRun;
+    };
 
-  return goalType;
+    return goalType;
 };
 
 export default {
-  getGoalType
+    getGoalType
 };
