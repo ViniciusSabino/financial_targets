4e3cb76c449fe21d4873bc0af0379ba591c34bf9
Vinicius Rocha <vnc.chelsea@gmail.com>
2019-12-14 16:38:48 -0300

    [accounts] test(services/find-accounts): create mongodb sort

--- accounts/src/domain/common/accounts.test.js
+++ accounts/src/domain/common/accounts.test.js
@@ -1,21 +1,16 @@
 import { setAccountStatus } from './accounts';
 import accountConstants from '../../utils/constants/account';
 
-/* eslint-disable max-nested-callbacks */
-describe('Domain', () => {
-    describe('Helpers', () => {
-        describe('Accounts', () => {
-            describe('setAccountStatus', () => {
-                it('return status "DONE" when amountPaid equals value', () => {
-                    const unpaidAccountMock = {
-                        value: 200,
-                        status: accountConstants.status.pending,
-                    };
-                    const status = setAccountStatus(200, unpaidAccountMock);
+describe('Common', () => {
+    describe('setAccountStatus', () => {
+        it('return status "DONE" when amountPaid equals value', () => {
+            const unpaidAccountMock = {
+                value: 200,
+                status: accountConstants.status.pending,
+            };
+            const status = setAccountStatus(200, unpaidAccountMock);
 
-                    expect(status).toEqual(accountConstants.status.done);
-                });
-            });
+            expect(status).toEqual(accountConstants.status.done);
         });
     });
 });
